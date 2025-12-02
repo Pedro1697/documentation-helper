042d7935bc4dceff1652c68dbb73548c763bf82e
Pedro1697 <agpn16@gmail.com>
2025-12-02 22:16:57 +0100

    chat message

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from backend.core import run_llm
 import streamlit as st 
-from types import Set 
 
 def main():
-    def create_sources_string(source_urls:Set[str]) -> str:
+    def create_sources_string(source_urls:set[str]) -> str:
         if not source_urls:
             return ""
         source_list = list(source_urls)
@@ -18,6 +17,12 @@ def main():
     st.header("Documentation Helper Bot")
 
     prompt = st.text_input("Prompt",placeholder="Enter your prompt here ...")
+
+    if "user_prompt_history" not in st.session_state:
+        st.session_state["user_prompt_history"] = []
+    
+    if "chat_answer_history"not in st.session_state:
+        st.session_state["chat_answer_history"] = []
     
     if prompt:
         with st.spinner("Generating response..."):
@@ -25,8 +30,16 @@ def main():
             sources = set([doc.metadata["source"] for doc in generated_response["source_documents"]])
 
             formatted_response = (
-                f"{generated_response['result']} \n\n {create_source_string(sources)}"
+                f"{generated_response['result']} \n\n {create_sources_string(sources)}"
             )
+
+            st.session_state["user_prompt_history"].append(prompt)
+            st.session_state["chat_answer_history"].append(formatted_response)
+
+    if st.session_state["chat_answer_history"]:
+        for generated_responses, user_query in zip(st.session_state["chat_answer_history"],st.session_state["user_prompt_history"]):
+            st.chat_message("user").write(user_query)
+            st.chat_message("assitant").write(generated_responses)
 
 
 if __name__ == "__main__":
