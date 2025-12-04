354fe7999b7b499fa8237d50203f7e5156aae114
Pedro1697 <agpn16@gmail.com>
2025-12-04 20:35:37 +0100

    adding memmory

--- backend/core.py
+++ backend/core.py
@@ -1,17 +1,18 @@
 from dotenv import load_dotenv
 from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever 
 from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_pinecone import PineconeVectorStore
 from langchain_ollama import OllamaEmbeddings
 from langchain_google_genai import GoogleGenerativeAI
-
+from typing import Dict,Any,  List
 
 load_dotenv()
 
 INDEX_NAME = "documentation-helper"
 
-def run_llm(query:str):
+def run_llm(query:str,chat_history:List[Dict[str,Any]]):
     embeddings = OllamaEmbeddings(model="mxbai-embed-large")
     docsearch = PineconeVectorStore(index_name=INDEX_NAME,embedding=embeddings)
     chat = GoogleGenerativeAI( model="gemini-2.5-flash")
@@ -19,11 +20,16 @@ def run_llm(query:str):
     retrival_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
     stuff_documents_chain = create_stuff_documents_chain(chat,retrival_qa_chat_prompt)
 
-    qa = create_retrieval_chain(
-        retriever=docsearch.as_retriever(),combine_docs_chain=stuff_documents_chain
+    rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
+    history_aware_retriever = create_history_aware_retriever(
+        llm=chat,retriever=docsearch.as_retriever(),prompt=rephrase_prompt
     )
 
-    result = qa.invoke(input={"input":query})
+    qa = create_retrieval_chain(
+        retriever=history_aware_retriever,combine_docs_chain=stuff_documents_chain
+    )
+
+    result = qa.invoke(input={"input":query,"chat_history":chat_history})
     new_result = {
         "query":result["input"],
         "result":result["answer"],
--- main.py
+++ main.py
@@ -18,15 +18,18 @@ def main():
 
     prompt = st.text_input("Prompt",placeholder="Enter your prompt here ...")
 
-    if "user_prompt_history" not in st.session_state:
-        st.session_state["user_prompt_history"] = []
-    
-    if "chat_answer_history"not in st.session_state:
+    if(
+        "chat_answer_history" not in st.session_state
+        and "user_prompt_history" not in st.session_state
+        and "chat_history" not in st.session_state
+    ):
         st.session_state["chat_answer_history"] = []
+        st.session_state["user_prompt_history"] = []
+        st.session_state["chat_history"] = []
     
     if prompt:
         with st.spinner("Generating response..."):
-            generated_response = run_llm(query=prompt)
+            generated_response = run_llm(query=prompt,chat_history=st.session_state["chat_history"])
             sources = set([doc.metadata["source"] for doc in generated_response["source_documents"]])
 
             formatted_response = (
@@ -35,6 +38,8 @@ def main():
 
             st.session_state["user_prompt_history"].append(prompt)
             st.session_state["chat_answer_history"].append(formatted_response)
+            st.session_state["chat_history"].append(("human",prompt))
+            st.session_state["chat_history"].append(("ai",generated_response["result"]))
 
     if st.session_state["chat_answer_history"]:
         for generated_responses, user_query in zip(st.session_state["chat_answer_history"],st.session_state["user_prompt_history"]):
